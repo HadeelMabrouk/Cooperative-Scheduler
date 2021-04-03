09724b1ae937b979c1241e318e1e4984df6d8bda
Hadeel Mabrouk <hadeelmabrouk97@gmail.com>
2021-04-03 20:08:12 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+void Dispatch(Node** head);
+void QueTask(Node** head,fptr f, int p);
+Node* Init(void);
+
+
+void Dispatch(Node** head)
+{
+    fptr e;
+    e = peek(head);
+    pop(head);
+    (*e)();
+}
+
+void QueTask(Node** head,fptr f, int p)
+{
+    push(head, f, p);
+}
+
+Node* Init(void)
+{
+    Node* pq = newNode(f1, 1);
+    pop(&pq);
+    return pq;
+}
+ 
+ 
+void f1(void) {
+    printf("1\n");
+}
+void f2(void) {
+    printf("2\n");
+}
+void f3(void) {
+    printf("3\n");
+}
+// Driver code
+int main()
+{
+    // Create a Priority Queue
+    // fn1->fn2->fn3
+    Node* pq = newNode(f1, 1);
+    //pop(&pq);
+    QueTask(&pq, f2, 2);
+    QueTask(&pq, f3, 3);
+ 
+    while (!isEmpty(&pq)) {
+        Dispatch(&pq);
+    }
+ 
+    return 0;
+}
