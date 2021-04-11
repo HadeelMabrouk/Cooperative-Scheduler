b6d4dc1070333ad669bccb315b54a6e71661c293
eman3asem <62911896+eman3asem@users.noreply.github.com>
2021-04-11 21:50:51 +0200

    Update main.c

--- Demo Applications/Parking Sensor/main.c
+++ Demo Applications/Parking Sensor/main.c
@@ -307,12 +307,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(Trig_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : echo_Pin */
-  GPIO_InitStruct.Pin = echo_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(echo_GPIO_Port, &GPIO_InitStruct);
-
   /*Configure GPIO pin : PB5 */
   GPIO_InitStruct.Pin = GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
