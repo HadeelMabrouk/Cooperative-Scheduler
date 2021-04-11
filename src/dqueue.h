65d9c786ca986dd17635abc3d9441a3869156b83
Hadeel Mabrouk <hadeelmabrouk97@gmail.com>
2021-04-11 02:26:24 +0200

    updated queue functions

--- src/dqueue.h
+++ src/dqueue.h
@@ -3,6 +3,8 @@
 
 // C code to implement Priority Queue
 // using Linked List
+#include <stdio.h>
+#include <stdlib.h>
  
 // Node
 typedef void (*fptr)(void);
@@ -24,81 +26,6 @@ NodeD* peekD(NodeD** head);
 void popD(NodeD** head);
 void pushD(NodeD** head, fptr f, int p, int d);
 int isEmptyD(NodeD** head);
-
-// Function to Create A New Node
-NodeD* newNodeD(fptr f, int p, int d)
-{
-    NodeD* temp = (NodeD*)malloc(sizeof(NodeD));
-    temp->fn = f;
-    temp->priority = p;
-    temp->delay = d;
-    temp->next = NULL;
- 
-    return temp;
-}
- 
-// Return the value at head
-NodeD* peekD(NodeD** head)
-{
-    return (*head);
-}
- 
-// Removes the element with the
-// highest priority form the list
-void popD(NodeD** head)
-{
-    NodeD* temp = *head;
-    (*head) = (*head)->next;
-    free(temp);
-}
- 
-// Function to push according to priority
-void pushD(NodeD** head, fptr f, int p, int d)
-{
-    NodeD* start = (*head);
-
-    // Create new Node
-    NodeD* temp = newNodeD(f, p, d);
- 
-    if(!isEmptyD(head))
-    {
-        // Special Case: The head of list has lesser
-        // priority than new node. So insert new
-        // node before head node and change head node.
-        if ((*head)->delay > d) {
-     
-            // Insert New Node before head
-            temp->next = *head;
-            (*head) = temp;
-        }
-        else {
-     
-            // Traverse the list and find a
-            // position to insert new node
-            while (start->next != NULL &&
-                start->next->delay < d) {
-                start = start->next;
-            }
-     
-            // Either at the ends of the list
-            // or at required position
-            temp->next = start->next;
-            start->next = temp;
-        }
-    }
-    else
-    {
-        (*head) = temp;
-    }
-    
-}
- 
-// Function to check is list is empty
-int isEmptyD(NodeD** head)
-{
-    return (*head) == NULL;
-}
-
 
 
 #endif
