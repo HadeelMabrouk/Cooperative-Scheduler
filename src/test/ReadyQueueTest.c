3d61c3034e0837ee8ed2600c66fea6668b45854c
Hadeel Mabrouk <hadeelmabrouk97@gmail.com>
2021-04-11 02:29:25 +0200

    Update and rename queue_test.c to ReadyQueueTest.c

---
+++ src/test/ReadyQueueTest.c
@@ -0,0 +1,30 @@
+#include "scheduler.h"
+ 
+ 
+void t1(void) {
+    printf("1\n");
+}
+void t2(void) {
+    printf("2\n");
+}
+void t3(void) {
+    printf("3\n");
+}
+// Driver code
+int main()
+{
+    // Create a Priority Queue
+    // fn1->fn2->fn3
+    Init();
+        
+    QueTask(t1, 1);
+    QueTask(t2, 2);
+    QueTask(t3, 3);
+    
+
+  while (1) 
+  {
+        Dispatch();
+  }
+  return 0;
+}
